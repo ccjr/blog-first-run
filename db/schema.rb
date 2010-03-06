dfc001536316e723a3f000179b532512f054aa7f
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2010-03-06 16:11:20 -0500

    Ran migrations for User and Profile

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100223232337) do
+ActiveRecord::Schema.define(:version => 20100306210440) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,26 @@ ActiveRecord::Schema.define(:version => 20100223232337) do
     t.datetime "updated_at"
     t.string   "excerpt"
     t.string   "location"
+    t.string   "author"
+    t.integer  "author_id"
+  end
+
+  create_table "profiles", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.date     "birthday"
+    t.text     "bio"
+    t.string   "colour"
+    t.string   "twitter"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
