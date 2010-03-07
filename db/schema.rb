25812f0afd19b5a15b359e003a9dc75bb3347ada
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2010-03-06 19:17:35 -0500

    Added category model

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100306221123) do
+ActiveRecord::Schema.define(:version => 20100307001138) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(:version => 20100306221123) do
     t.string   "author"
     t.integer  "author_id"
     t.integer  "user_id"
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "profiles", :force => true do |t|
