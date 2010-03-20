3a04c66a3ba177f06d4a69ff4e90fd79cf2c9dd5
rbarazi <rbarazi@gmail.com>
2010-03-20 17:38:26 -0400

    Adding new and create methods for session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,11 @@
 class SessionsController < ApplicationController
+  def create
+    if user = User.authenticate(params[:login], params[:password])
+      session[:user_id] = user.id
+      redirect_to articles_path, :notice => "Logged in successfully"
+    else
+      flash.now[:alert] = "Invalid login/password combination"
+      render :action => 'new'
+    end
+  end
 end
