db9f847bbecc2eff3b5c6d61508d26607c258cc9
rbarazi <rbarazi@gmail.com>
2010-03-25 22:45:55 -0400

    Try rails 3 ajax stuff

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,25 @@ class CommentsController < ApplicationController
   def create
     @comment = @article.comments.new(params[:comment])
     if @comment.save
-      redirect_to @article, :notice => 'Thanks for your comment'
+      respond_to do |format|
+        format.html { redirect_to @article, :notice => 'Thanks for your comment' }
+        format.js
+      end
     else
-      redirect_to @article, :alert => 'Unable to add comment'
+      respond_to do |format|
+        format.html { redirect_to @article, :alert => 'Unable to add comment' }
+        format.js
+      end
     end
   end
   
   def destroy
     @comment = @article.comments.find(params[:id])
     @comment.destroy
-    redirect_to @article, :notice => 'Comment deleted'
+    respond_to do |format|
+      format.html { redirect_to @article, :notice => 'Comment deleted' }
+      format.js
+    end
   end
   
   private
