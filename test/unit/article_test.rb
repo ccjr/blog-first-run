dfbb221f68276531c5d23e14ee25e9fe0e1a6d12
rbarazi <rbarazi@gmail.com>
2010-04-06 17:34:28 -0400

    Added articles tests

--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should create article" do
+    article = Article.new
+
+    article.user  = users(:eugene)
+    article.title = "Test article"
+    article.body  = "Test body"
+
+    assert article.save
+  end
+  
+  test "should find article" do
+    article_id = articles(:welcome_to_rails).id
+    assert_nothing_raised { Article.find(article_id) }
   end
 end
