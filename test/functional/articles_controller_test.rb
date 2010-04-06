c705ff2655c1c0094b8f1bebd30e9bd35cc0574e
rbarazi <rbarazi@gmail.com>
2010-04-06 19:28:05 -0400

    Fixed all articles controller tests

--- test/functional/articles_controller_test.rb
+++ test/functional/articles_controller_test.rb
@@ -4,42 +4,59 @@ class ArticlesControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_template 'index'
     assert_not_nil assigns(:articles)
   end
 
   test "should get new" do
+    login_as(:eugene)
     get :new
     assert_response :success
   end
 
   test "should create article" do
+    login_as(:eugene)
     assert_difference('Article.count') do
-      post :create, :article => articles(:one).attributes
-    end
+      post :create, :article => { :title => 'Post title',
+                                  :body  => 'Lorem ipsum..' }
+      end
 
+    assert_response :redirect
     assert_redirected_to article_path(assigns(:article))
   end
 
   test "should show article" do
-    get :show, :id => articles(:one).to_param
+    get :show, :id => articles(:welcome_to_rails).to_param
+
     assert_response :success
+    assert_template 'show'
+
+    assert_not_nil assigns(:article)
+    assert assigns(:article).valid?
   end
 
   test "should get edit" do
-    get :edit, :id => articles(:one).to_param
+    login_as(:eugene)
+    get :edit, :id => articles(:welcome_to_rails).to_param
     assert_response :success
   end
 
   test "should update article" do
-    put :update, :id => articles(:one).to_param, :article => articles(:one).attributes
+    login_as(:eugene)
+    put :update, :id => articles(:welcome_to_rails).to_param, :article => articles(:welcome_to_rails).attributes
     assert_redirected_to article_path(assigns(:article))
   end
 
   test "should destroy article" do
-    assert_difference('Article.count', -1) do
-      delete :destroy, :id => articles(:one).to_param
-    end
+    login_as(:eugene)
+    assert_nothing_raised { Article.find(articles(:welcome_to_rails).to_param) }
 
+    assert_difference('Article.count', -1) do
+      delete :destroy, :id => articles(:welcome_to_rails).to_param
+    end
+    assert_response :redirect
     assert_redirected_to articles_path
+
+    assert_raise(ActiveRecord::RecordNotFound) { Article.find(articles(:welcome_to_rails).to_param) }
   end
 end
