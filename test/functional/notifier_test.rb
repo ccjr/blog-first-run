03d9e19038000efe6513a9ae387c1d739e1e48a6
rbarazi <rbarazi@gmail.com>
2010-04-06 19:39:32 -0400

    @message is just message now

--- test/functional/notifier_test.rb
+++ test/functional/notifier_test.rb
@@ -3,10 +3,10 @@ require 'test_helper'
 class NotifierTest < ActionMailer::TestCase
   test "email_friend" do
     article = articles(:welcome_to_rails)
-    @message = Notifier.email_friend(article, 'Josh Smith', 'dude@example.com')\
+    message = Notifier.email_friend(article, 'Josh Smith', 'dude@example.com')
     
-    assert_equal "Interesting Article", @message.subject
-    assert_equal ["dude@example.com"], @message.to
-    assert_equal ["from@example.com"], @message.from
+    assert_equal "Interesting Article", message.subject
+    assert_equal ["dude@example.com"], message.to
+    assert_equal ["from@example.com"], message.from
   end
 end
